9612fc3382df8a7e7f682f8dee8474c8fc316dd1
mic050r <103114387+mic050r@users.noreply.github.com>
2023-05-23 08:58:56 +0900

    예제 8-10 [chore]  : 코멘트 추가

--- src/file03/BinaryCopyEx.java
+++ src/file03/BinaryCopyEx.java
@@ -8,21 +8,24 @@ import java.io.IOException;
 public class BinaryCopyEx {
 
 	public static void main(String[] args) {
-//		File src = new File("./src/file03/forest.jpg");
-//		File dest = new File("./src/file03/copyforest.jpg");
-		String src = ("./src/file03/forest.jpg");
-		String dest = ("./src/file03/copyforest.jpg");
+		File src = new File("./src/file03/forest.jpg");
+		// src파일 객체를 생성하면 파일에 대한 정보를 얻을 수 있다.
+		File dest = new File("./src/file03/copyforest.jpg");
+//		String src = ("./src/file03/forest.jpg");
+//		String dest = ("./src/file03/copyforest.jpg");
 		int c;
 		try {
 			FileInputStream fi = new FileInputStream(src);
+			// fi 입력객체는 src객체의 파일(forest.jpg)을 가리킨다.
 			FileOutputStream fo = new FileOutputStream(dest);
-			while ((c = fi.read()) != -1) {
-				fo.write((byte) c);
+			// fo출력객체는 dest객체의 파일(copyforest.jpg)을 가리킨다.
+			while ((c = fi.read()) != -1) { // forest.jpg파일에서 1바이트씩 읽음
+				fo.write((byte) c); // 1바이트씩 copyforest.jpg 파일에 출력한다.
 			}
-			fi.close();
 			fo.close();
-//			System.out.println(src.getPath() + "를 " + dest.getPath() + "로 복사하였습니다.");
-			System.out.println("복사가 잘되었음");
+			fi.close();
+			System.out.println(src.getPath() + "를 " + dest.getPath() + "로 복사하였습니다.");
+//			System.out.println("복사가 잘되었음");
 		} catch (IOException e) {
 			System.out.println("파 일복사 오류");
 		}
